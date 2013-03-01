100af2edf83b780dd5fb1ad6345aa01a2b50e146
BLYTH3 <ST4R13UCK@gmail.com>
2013-03-01 20:35:35 +0700

    update app

--- grunt.js
+++ grunt.js
@@ -1,12 +1,9 @@
 module.exports = function(grunt) {
-
-	  grunt.initConfig({
-		      lint: {
-						      files: ['hello.js']
-		      }
-					  });
-
-	    grunt.registerTask('default', 'lint');
-		  grunt.registerTask('travis', 'lint');
-
+    grunt.initConfig({
+     lint: {
+      files: ['hello.js']
+    }
+  });
+  grunt.registerTask('default', 'lint');
+  grunt.registerTask('travis', 'lint');
 };
